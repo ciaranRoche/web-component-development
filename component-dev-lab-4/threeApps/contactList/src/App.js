240db3d4b4ab0078f50b83f5b0e04d3327457328
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-10-03 10:56:08 +0100

    contact list - static version added

--- component-dev-lab-4/threeApps/contactList/src/App.js
+++ component-dev-lab-4/threeApps/contactList/src/App.js
@@ -1,21 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class ContactForm extends Component {
+  render(){
+    return (
+      <tr>
+        <td>
+          <input type="text" className="form-control" />
+        </td> 
+        <td>
+          <input type="text" className="form-control"/>
+        </td>
+        <td>
+          <input type="text" className="form-control" />
+        </td>
+        <td>
+          <input type="button" className="btn btn-primary" value="Add"/>
+        </td>
+      </tr>
+    )
+  }
+}
+
+class Contact extends Component {
+  render(){
+    return (
+      <tr>
+        {/* TODO */}
+      </tr>
+    )
+  }
+}
+
+class ContactList extends Component {
+  render(){
+    let contactRows = null; //TODO
+    return (
+      <tbody>
+        {contactRows}
+        <ContactForm />
+      </tbody>
+    )
+  }
+}
+
+class ContactsTable extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <ContactList contacts={this.props.contacts}/>
+      </table>
+    );
+  }
+}
+
+class ContactsApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contact List.</h1>
+        <ContactsTable contacts={this.props.contacts}/>
       </div>
     );
   }
 }
 
-export default App;
+export default ContactsApp;
+
+
