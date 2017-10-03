6a051c5fad6cd6c6441d536ce484bd9fdfabbbda
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-10-03 12:11:02 +0100

    hacker news - static version - no data set rendered

--- component-dev-lab-4/threeApps/hackerNews/src/App.js
+++ component-dev-lab-4/threeApps/hackerNews/src/App.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class Form extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <form style={{marginTop: '30px'}}>
+        <h3>Add a new post</h3>
+        <div className='form-group'>
+          <input type='text'
+            className='form-control'
+            placeholder='title'></input>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='form-group'>
+          <input type='text'
+            className='form-control'
+            placeholder='Link'></input>
+        </div>
+        <button type='submit' className='btn btn-primary'>Post</button>
+      </form>
+    );
+  }
+}
+
+class NewsItem extends Component {
+  render() {
+    let divStyle = {
+      fontSize: '20px', 
+      marginLeft: '10px' 
+    };
+    let cursor = { cursor: 'pointer' } ;
+    let line ;
+    if (this.props.post.link ) {
+      line = <a href={this.props.post.link} >
+        {this.props.post.title}</a>;
+      } else {
+        line = <span>{this.props.post.title} </span> ;
+      }
+      return (
+        <div>
+          <span className="glyphicon glyphicon-thumbs-up" style={cursor} />
+            {this.props.post.upvotes}
+          <span style={divStyle}>{line}<span>
+            <a href={'#/posts/' + this.props.post.id }>Comments</a>
+          </span>
+          </span>
+        </div>  
+      );
+    }
+}
+
+class NewsList extends Component {
+  render() {
+    return null; //TODO
+  }
+}
+
+class HackerApp extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6 col-md-offset-3'>
+            <div className='page-header'>
+              <h1>Hacker News</h1>
+              <NewsList />
+              <Form/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default HackerApp;
