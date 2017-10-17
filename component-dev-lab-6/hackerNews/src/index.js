d35a4029871b41cee013ce3d707a5e448b7cb700
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-10-17 12:02:39 +0100

    pitstop

---
+++ component-dev-lab-6/hackerNews/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HackerApp from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+
+ReactDOM.render(
+  <HackerApp/>,
+  document.getElementById('root')
+);
