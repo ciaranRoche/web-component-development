ac437103aa1d8af42fb863988846dda7c1ba4bf5
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-11-11 14:11:20 +0000

    add product resource

---
+++ component-dev-lab-7/webapilab/webapilab/api/products/products.controller.js
@@ -0,0 +1,31 @@
+var datastore = require('../datastore');
+
+// Get all product
+exports.index = function (req, res) {
+  return res.json(200, datastore.products);
+};
+
+// Get a single product
+exports.show = function (req, res) {
+  if (datastore.products[req.params.code]) {
+    return res.json(datastore.products[req.params.code]);
+  } else {
+    return res.send(404);
+  }
+
+};
+
+// Creates a new product.
+exports.create = function (req, res) {
+  var product = {
+    id: req.params.code,
+    name: req.body.name,
+    description: req.body.description
+  };
+  if (datastore.products[req.params.code]) {
+    return res.send(409, 'Id already exists');
+  } else {
+    datastore.products[req.params.code] = product;
+    return res.json(201, product);
+  }
+};
